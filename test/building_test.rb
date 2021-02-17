bb93050fb0f6a01168aa2a64d697bc9c8dfa060a
Carlyle Douglas <cdouglascorp@gmail.com>
2021-02-17 10:33:16 -0700

    Test building has no rented units by default

--- test/building_test.rb
+++ test/building_test.rb
@@ -4,13 +4,16 @@ require './lib/building'
 require './lib/apartment'
 require './lib/renter'
 
+
 class BuildingTest < Minitest::Test
   def setup
     @building = Building.new
     @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
-    @renter1 = Renter.new("Aurora")
-    @renter2 = Renter.new("Tim")
+    @unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    @unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    # @renter1 = Renter.new("Aurora")
+    # @renter2 = Renter.new("Tim")
   end
 
   def test_it_exists
@@ -33,17 +36,22 @@ class BuildingTest < Minitest::Test
   end
 
   def test_it_can_add_a_renter
+    renter1 = Renter.new("Aurora")
+
     @building.add_unit(@unit1)
-    @unit1.add_renter(@renter1)
+    @unit1.add_renter(renter1)
 
     assert_equal ["Aurora"], @building.renters
   end
 
   def test_it_can_add_multiple_renters
+    renter1 = Renter.new("Aurora")
+    renter2 = Renter.new("Tim")
+
     @building.add_unit(@unit1)
     @building.add_unit(@unit2)
-    @unit1.add_renter(@renter1)
-    @unit2.add_renter(@renter2)
+    @unit1.add_renter(renter1)
+    @unit2.add_renter(renter2)
 
     assert_equal ["Aurora", "Tim"], @building.renters
   end
@@ -53,5 +61,15 @@ class BuildingTest < Minitest::Test
     @building.add_unit(@unit2)
 
     assert_equal 1099.5, @building.average_rent
+  end
+
+  def test_it_has_no_rented_units_by_default
+    renter1 = Renter.new("Spencer")
+
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
+
+    assert_equal [], @building.rented_units
   end
 end
