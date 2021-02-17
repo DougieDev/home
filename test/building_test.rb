d2238e3316a178d921ca474de96a110675997148
Carlyle Douglas <cdouglascorp@gmail.com>
2021-02-17 10:17:13 -0700

    Test building can calc average rent

--- test/building_test.rb
+++ test/building_test.rb
@@ -46,6 +46,12 @@ class BuildingTest < Minitest::Test
     @unit2.add_renter(@renter2)
 
     assert_equal ["Aurora", "Tim"], @building.renters
+  end
 
+  def test_it_can_calculate_avg_rent
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+
+    assert_equal 1099.5, @building.average_rent
   end
 end
