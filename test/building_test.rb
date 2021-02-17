ce470f5981c3a2de5da8afa207b53939bcdfda96
Carlyle Douglas <cdouglascorp@gmail.com>
2021-02-17 09:49:58 -0700

    Add setup and test for instance

--- test/building_test.rb
+++ test/building_test.rb
@@ -5,4 +5,11 @@ require './lib/apartment'
 require './lib/renter'
 
 class BuildingTest < Minitest::Test
+  def setup
+    @building = Building.new
+  end
+
+  def test_it_exists
+    assert_instance_of Building, @building
+  end
 end
