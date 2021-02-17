89e211c9caf61d66b8b0d2d59cb42460ce739c71
Carlyle Douglas <cdouglascorp@gmail.com>
2021-02-17 09:35:28 -0700

    Add setup for apartment test file

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -4,18 +4,16 @@ require './lib/apartment'
 require './lib/renter'
 
 class ApartmentTest < Minitest::Test
-  # def setup
-  #   unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-  # end
+  def setup
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  end
 
   def test_it_exists
-    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-    assert_instance_of Apartment, unit1
+    assert_instance_of Apartment, @unit1
   end
 
   def test_it_has_a_unit_number
-    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-    assert_equal "A1", unit1.number
-    # require 'pry';binding.pry
+    assert_equal "A1", @unit1.number
+
   end
 end
