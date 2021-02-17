4b1ce45f96e5f4dc5fe5923d56256a69306f4fa7
Carlyle Douglas <cdouglascorp@gmail.com>
2021-02-17 10:12:01 -0700

    Test that it can add multiple renters

--- test/building_test.rb
+++ test/building_test.rb
@@ -10,6 +10,7 @@ class BuildingTest < Minitest::Test
     @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     @renter1 = Renter.new("Aurora")
+    @renter2 = Renter.new("Tim")
   end
 
   def test_it_exists
@@ -36,5 +37,15 @@ class BuildingTest < Minitest::Test
     @unit1.add_renter(@renter1)
 
     assert_equal ["Aurora"], @building.renters
+  end
+
+  def test_it_can_add_multiple_renters
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @unit1.add_renter(@renter1)
+    @unit2.add_renter(@renter2)
+
+    assert_equal ["Aurora", "Tim"], @building.renters
+
   end
 end
