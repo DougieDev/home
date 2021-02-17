6993034f2a238497b189d8a5958ff703a55d2f04
Carlyle Douglas <cdouglascorp@gmail.com>
2021-02-17 09:32:09 -0700

    Add apartment test for instance and unit number

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -4,4 +4,18 @@ require './lib/apartment'
 require './lib/renter'
 
 class ApartmentTest < Minitest::Test
+  # def setup
+  #   unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  # end
+
+  def test_it_exists
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_instance_of Apartment, unit1
+  end
+
+  def test_it_has_a_unit_number
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_equal "A1", unit1.number
+    # require 'pry';binding.pry
+  end
 end
